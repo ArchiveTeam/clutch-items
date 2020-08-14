7b621c7f18c36aadd8c2b1519221c2c1a5d05166
arkiver <arkiver@protonmail.com>
2020-08-13 20:15:56 -0400

    Final fours lists processed. All items added to clutchvideos.

--- raw/get-raw.sh
+++ raw/get-raw.sh
@@ -4,4 +4,10 @@ wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/4e0MV/5M-2-po
 wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/CcPh4/5M-2-users-deduped.jsonl.gz
 wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/10lqdC/5M-3-posts.jsonl.gz
 wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/WfC5b/5M-3-users-deduped.jsonl.gz
+wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/NJHF9/5M-4-posts.jsonl.gz
+wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/nhY0z/5M-4-users-deduped.jsonl.gz
+wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/9kbSl/update-20200812T2325Z-posts.jsonl.gz
+wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/SDleU/update-20200812T2325Z-users-deduped.jsonl.gz
+wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/B9AQx/update-20200813T2155Z-posts.jsonl.gz
+wget https://web.archive.org/web/2id_/https://transfer.notkiska.pw/FA3v8/update-20200813T2155Z-users-deduped.jsonl.gz
 gzip -d *.gz
